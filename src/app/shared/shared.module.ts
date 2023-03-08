b6ddf54c4873a29b94fddc8e5d2dd1e5a2fd9421
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-03-08 11:37:23 -0300

    Feat: add router link

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,6 +10,6 @@ import { PokeListComponent } from './poke-list/poke-list.component';
 @NgModule({
   declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
