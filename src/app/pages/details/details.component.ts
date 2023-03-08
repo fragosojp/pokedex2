9d5676b5867bb3510446e8072cd2a799701e528d
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-03-08 12:39:02 -0300

    Feat:  config details with forkJoin() do rxjs

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/pokeApi.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+  ngOnInit(): void {
+    this.pokemon;
+  }
+  get pokemon() {
+    //Pegar o id da url
+    const id = this.activatedRoute.snapshot.params['id'];
+    //pegar o pokemon da api
+    const pokemon = this.pokeApiService.apiGetPokemon(
+      `${this.urlPokemon}/${id}`
+    );
+    //Pegar a especie do pokemon da api
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
 
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
