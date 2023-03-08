9affcab3a724019f92a5d871dc945e3d1f7acc2d
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-03-08 11:48:30 -0300

    Feat: add EventEmitter for search

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
   templateUrl: './poke-search.component.html',
   styleUrls: ['./poke-search.component.scss'],
 })
-export class PokeSearchComponent {}
+export class PokeSearchComponent {
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+
+  public search(value: string) {
+    this, this.emmitSearch.emit(value);
+  }
+}
